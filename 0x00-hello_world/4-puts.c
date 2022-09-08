12166eafd9d22de63c85776a1b4540ef86b36b57
Joshua Nartey <narteyj8@gmail.com>
2022-09-08 16:58:15 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * 
+ * Return: ALways returns 0 for smooth execution
+ 
+int main(void)
+{
+puts("\"Programming is like buuilding a multilingual puzzle")
+return (0);
+}
