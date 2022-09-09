bd2b20268a746cbcae8ef48277f02c533513de4d
Joshua Nartey <narteyj8@gmail.com>
2022-09-08 17:01:28 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,11 @@
  * main- entry point
  * 
  * Return: ALways returns 0 for smooth execution
+ */
  
 int main(void)
 {
 puts("\"Programming is like buuilding a multilingual puzzle")
 return (0);
 }
+
