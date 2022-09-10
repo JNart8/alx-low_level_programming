163316ed8e57984c0a8a53a495746596927a3857
Joshua Nartey <narteyj8@gmail.com>
2022-09-09 23:12:15 -0700

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints all possible combinations of single-digit numbers 
+ * main - prints all possible combinations of single-digit numbers
  * Return: returns 0 for success
  */
 int main(void)
