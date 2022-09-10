574b809d002adf87da3e4ccc56cb608869a8d415
Joshua Nartey <narteyj8@gmail.com>
2022-09-10 00:52:20 -0700

    2 digit combo all

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-/*
+/**
  * main -prints all possible combo of 2 digit numbers
  * Return: 0 for success
  */
 int main(void)
 {
-	int i,j;
-	
+	int i, j;
+
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			if(i < j)
+			if (i < j)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
