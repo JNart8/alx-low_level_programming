99958c0e1cc51bb6b706b1575db484d77aa140f7
Joshua Nartey <narteyj8@gmail.com>
2022-09-10 00:31:25 -0700

    3 digit combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - prints all possible different combinations of three digits
- *   * Return: Always 0 (Success)
- *    */
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n, m, l;
@@ -28,7 +28,7 @@ int main(void)
 			}
 		}
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
 
