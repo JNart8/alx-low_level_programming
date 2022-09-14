a8ba33187a0d9b5518c150fc640b9b8906790219
Joshua Nartey <narteyj8@gmail.com>
2022-09-13 18:16:40 -0700

    10x lower alphabeets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int iteration = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		iteration++;
+	}
+}
+
