8f795b86dabbbbb0b3d794d560d9c1c47c93284f
Joshua Nartey <narteyj8@gmail.com>
2022-09-14 16:49:12 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,6 +25,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar((res % 10) + '0');
 				}
 				else if (res >= 10 && res < 100)
