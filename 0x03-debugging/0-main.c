4b19745d7fcbe5a87b0bb5d90fb1dc9341593752
Joshua Nartey <narteyj8@gmail.com>
2022-09-15 16:28:34 -0700

    positive and negetivre

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ *  main - tests function that prints if int is +ve/-ve
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	prositive_or_negetive(i);
+
+	return (0)
+}
