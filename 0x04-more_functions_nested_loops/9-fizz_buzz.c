824a0c7aa681a2ecabc458f237c4c84f832a8010
Joshua Nartey <narteyj8@gmail.com>
2022-09-15 23:28:33 -0700

    buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - program that prints either number
  * or fizz or buzz or fizzBuzz
  * 
  * Return: returns 0
  */
+
 int main(void)
 {
 	int num = 1;
@@ -13,7 +15,7 @@ int main(void)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if ((num % 3) == 0)
 		{
