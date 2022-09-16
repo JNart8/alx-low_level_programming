8cdf64790be437d85b0e651ca3c1b4ab66985d35
Joshua Nartey <narteyj8@gmail.com>
2022-09-15 18:57:27 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
-/*
- *  main - tests function that prints if int is +ve/-ve
- *
+
+/**
+ *  main - tests function that prints if integer is postive or negetive
  *  Return: 0
  */
 
 int main(void)
 {
 	int i;
-       
+	
 	i = 0;
 	prositive_or_negetive(i);
 
