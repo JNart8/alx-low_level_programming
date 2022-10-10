ffe2e42838a17abc42c3174dfa6c207c9a3314ea
Joshua Nartey <narteyj8@gmail.com>
2022-10-10 01:02:13 -0700

    defined struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
