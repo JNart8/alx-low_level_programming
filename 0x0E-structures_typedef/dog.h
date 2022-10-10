f14e7fe99ffbde731a109ea6d5deb04e935526d7
Joshua Nartey <narteyj8@gmail.com>
2022-10-10 01:13:54 -0700

    function prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,11 @@ typedef struct dog
 	char *owner;
 } dog_t;
 #endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
